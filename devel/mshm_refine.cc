35746a080c0ce3ec7a4e4db3e012082459bee955
gedeone-octave <none@none>
2013-06-12 22:07:22 +0000

    Take as input also a list of vertex to refine closer to the Octave C++ guidelines Added a demo

--- devel/mshm_refine.cc
+++ devel/mshm_refine.cc
@@ -1,3 +1,4 @@
+
 /* Copyright (C) 2013 Marco Vassallo
   
    This program is free software: you can redistribute it and/or modify
@@ -18,151 +19,167 @@
 #include <octave/oct.h>
 #include <octave/oct-map.h>
 
-DEFUN_DLD (mshm_refine, args, ,
-	   "-*- texinfo -*-\n\
-@deftypefn {Function File} @var{refined_mesh} @\n\
-mshm_refine(@var{mesh})\n\
-\n\
-Uniformly refine a mesh.\n\
-\n\
-@itemize @bullet\n\
-@item @var{mesh} is the mesh you want to be refined \n\
+DEFUN_DLD (mshm_refine, args, ,"-*- texinfo -*-\n\
+@deftypefn {Function File} {[@var{refined_mesh}]} = \
+mshm_refine (@var{mesh},@var{cell_marker}) \n\
+Refine a mesh\n\
+@itemize @bullet \n\
+@item The @var{mesh} is a PDE-tool like structures with matrix field (p,e,t).\n\
+@item The optional argument @var{cell_marker} is a list\n\
+containing the number of the cells you want to refine.\n\
+By default a uniform refinement is applied.\n\
 @end itemize\n\
-\n\
-WARNING: the @var{refined_mesh} is a new mesh, which doesn't share any \n\
-information with the original @var{mesh}.  \n\
-For example, border labels could be different.   \n\
+The output @var{refined_mesh} is a refined mesh with\n\
+the same structure as @var{mesh}\n\
 @seealso{msh3m_structured_mesh, msh2m_structured_mesh}\n\
 @end deftypefn")
 {
   int nargin = args.length ();
   octave_value_list retval;
+  dim_vector dims;
+  dims.resize (2);
 
-  if (nargin != 1)
-    {
-      print_usage ();
-    }
+  if (nargin < 1 || nargin > 2)
+    print_usage ();
   else
     {
       Octave_map a (args(0).map_value ());
       if (! error_state)
-	{
-	  Matrix p (a.contents (a.seek ("p"))(0).matrix_value ());
-	  Matrix t (a.contents (a.seek ("t"))(0).matrix_value ());
-	  if (! error_state)
-	    {
-	      dolfin::Mesh mesh;
-	      std::size_t D = p.rows ();
-	      dolfin::MeshEditor editor;
-	      editor.open (mesh, D, D);
+        {
+          Array<double> p (a.contents (a.seek ("p"))(0).matrix_value ());
+          Array<octave_idx_type> t (a.contents (a.seek ("t"))(0).matrix_value ());
+          if (! error_state)
+            {
+              dolfin::Mesh mesh;
+              std::size_t D = p.rows ();
 
-	      std::size_t num_v = p.cols ();
-	      editor.init_vertices (num_v);
+              if (D < 2 || D > 3)
+                error ("mshm_refine: only 2D or 3D meshes are supported");
+              else
+                {
+                  dolfin::MeshEditor editor;
+                  editor.open (mesh, D, D);
+                  editor.init_vertices (p.cols ());
+                  editor.init_cells (t.cols ());
 
-	      std::size_t num_c = t.cols ();
-	      editor.init_cells (num_c);
+                  if (D == 2)
+                    {
+                      for (uint i = 0; i < p.cols (); ++i)
+                        editor.add_vertex (i, p.xelem (0, i), p.xelem (1, i));
 
-	      if (D == 2)
-		{
-		  for (uint i = 0; i < num_v; ++i)
-		    editor.add_vertex (i, p(0,i), p(1,i));
-      
-		  for (uint i = 0; i < num_c; ++i)
-		    editor.add_cell (i, t(0,i)-1, t(1,i)-1, t(2,i)-1);
-		}
+                      for (uint i = 0; i < t.cols (); ++i)
+                        editor.add_cell (i, t.xelem (0, i) - 1, t.xelem (1, i) - 1,
+                                            t.xelem (2, i) - 1);
+                    }
 
-	      if (D == 3)
-		{
-		  for (uint i = 0; i < num_v; ++i)
-		    editor.add_vertex (i, p(0,i), p(1,i), p(2,i));
-      
-		  for (uint i = 0; i < num_c; ++i)
-		    editor.add_cell (i, t(0,i)-1, t(1,i)-1, t(2,i)-1, t(3,i)-1);
-		}
+                  if (D == 3)
+                    {
+                      for (uint i = 0; i < p.cols (); ++i)
+                        editor.add_vertex (i, p.xelem (0, i), p.xelem (1, i), p.xelem (2, i));
 
-	      editor.close ();
+                      for (uint i = 0; i < t.cols (); ++i)
+                        editor.add_cell (i, t.xelem (0, i) - 1, t.xelem (1, i) - 1,
+                                            t.xelem (2, i) - 1, t.xelem (3, i) - 1);
+                    }
 
-	      dolfin::Mesh refined_mesh;
-	      refined_mesh = dolfin::refine(mesh);
+                  editor.close ();
 
-	      std::size_t num_v_r = refined_mesh.num_vertices ();
-	      Matrix p_r (D, num_v_r);
-	      std::vector<double> my_coord_r = refined_mesh.coordinates ();
-	      std::size_t n = 0;
+                  if (nargin == 2)
+                    { 
+                      Array<octave_idx_type> cell_idx (args(1).array_value ());
+                      if (! error_state)
+                        {
+                          dolfin::CellFunction<bool> cell_markers (mesh);
+                          cell_markers.set_all (false);
 
-	      for (octave_idx_type j=0; j < num_v_r; ++j)
-		for (octave_idx_type i =0; i < D; ++i,++n)
-		  p_r(i,j) = my_coord_r[n];
-	    
+                          for (octave_idx_type i = 0; i < cell_idx.length (); ++i)
+                            cell_markers.set_value (cell_idx.xelem (i) - 1, true);
 
-	      refined_mesh.init (D - 1, D);
-	      std::size_t num_f_r = refined_mesh.num_facets ();
-	      Matrix e_r;
-	      if(D == 2)
-		e_r.resize (7, num_f_r);
-	      if(D == 3)
-		e_r.resize (10, num_f_r);
-
-	      octave_idx_type l=0;
-	      octave_idx_type m=0;
-
-	      for (dolfin::FacetIterator f (refined_mesh); !f.end (); ++f)
-		{
-		  if ( (*f).exterior () == true)
-		    {
-		      l = 0;
-		      for (dolfin::VertexIterator v (*f); !v.end (); ++v,++l)
-			{ 
-			  e_r(l,m) = (*v).index () + 1 ;
-			}
-		      ++m;
-		    }   
-		}
-
-	      if (D == 2) 
-		{
-		  e_r.resize (7, m);
-		  e_r.fill (1, 6, 0, 6, m-1);
-		}
-
-	      if (D == 3) 
-		{
-		  e_r.resize (10, m);
-		  e_r.fill (1, 9, 0, 9, m-1);
-		}
-  
-	  
-	      std::size_t num_c_r = refined_mesh.num_cells ();
-	      Matrix t_r (D+2, num_c_r);
-	      t_r.fill (1.0);
-	      std::vector<unsigned int> my_cells_r = refined_mesh.cells ();
-	      n = 0;
-
-	      for (octave_idx_type j=0; j < num_c_r; ++j)
-		for (octave_idx_type i =0; i < D+1; ++i,++n)
-		  t_r(i,j) += my_cells_r[n]; 
-
-	      Octave_map a_r;
-	      a_r.assign ("p", octave_value (p_r));
-	      a_r.assign ("e", octave_value (e_r));
-	      a_r.assign ("t", octave_value (t_r));
-	      retval = octave_value (a_r);
+                          mesh = dolfin::refine (mesh, cell_markers);
+                        }
+                    }
+                  else
+                    mesh = dolfin::refine(mesh);
 
 
-	    }
-	}	
+                  std::size_t num_v = mesh.num_vertices ();
+                  dims(0) = D;
+                  dims(1) = num_v;
+                  p.resize (dims);
+                  std::copy (mesh.coordinates ().begin (),
+                             mesh.coordinates ().end (),
+                             p.fortran_vec ());
+                
+                  mesh.init (D - 1, D);
+                  std::size_t num_f = mesh.num_facets ();
+                
+                  // e has 7 rows in 2d, 10 rows in 3d
+                  dims(0) = D == 2 ? 7 : 10; 
+                  dims(1) = num_f;
+                  Array<octave_idx_type> e (dims, 0);
+                  octave_idx_type *evec = e.fortran_vec ();
+
+                  octave_idx_type l = 0, m = 0;
+
+                  for (dolfin::FacetIterator f (mesh); ! f.end (); ++f)
+                    {
+                      if ((*f).exterior () == true)
+                        {
+                          l = 0;
+                          for (dolfin::VertexIterator v (*f); ! v.end (); ++v, ++l) 
+                            e.xelem (l, m) = (*v).index () + 1 ;
+                          ++m;
+                        }
+                    }
+
+                  dims(1) = m;
+                  e.resize (dims);
+
+                  for (octave_idx_type j = e.rows () - 2; 
+                       j < e.numel () - 2; j += e.rows ())
+                    evec[j] = 1;
+
+                  dims(0) = D + 2;
+                  dims(1) =  mesh.num_cells ();
+                  t.clear ();
+                  t.resize (dims, 1);
+
+                  std::vector<unsigned int> my_cells = mesh.cells ();
+                  std::size_t n = 0;
+
+                  for (octave_idx_type j = 0; j < t.cols (); ++j)
+                    for (octave_idx_type i = 0; i < D + 1; ++i, ++n)
+                      t.xelem (i, j) += my_cells[n]; 
+
+                  Octave_map a;
+                  a.assign ("p", p);
+                  a.assign ("e", e);
+                  a.assign ("t", t);
+                  retval = octave_value (a);
+
+
+                }
+            }
+        }
     }
+
   return retval;
 }
 
 
 /*
 %!demo
-%! msh = msh2m_structured_mesh(linspace(0,1,3),linspace(0,1,3),1,[1:4]);
-%! refined_mesh = mshm_refine(msh);
+%! msh = msh2m_structured_mesh (linspace (0, 1, 4), linspace (0, 1, 4), 1, [1 : 4]);
+%! partially_refined_mesh = mshm_refine (msh,[1:3]);
+%! uniformly_refined_mesh = mshm_refine (msh);
 %! clf;
-%! subplot(1,2,1);
-%! msh2p_mesh(msh);
-%! subplot(1,2,2);
-%! msh2p_mesh(refined_mesh);
+%! subplot (1, 3, 1);
+%! msh2p_mesh (msh);
+%! title ('original mesh');
+%! subplot (1, 3, 2);
+%! msh2p_mesh (partially_refined_mesh);
+%! title ('partially refined mesh');
+%! subplot (1, 3, 3);
+%! msh2p_mesh (uniformly_refined_mesh);
+%! title ('uniformly refined mesh');
 */
